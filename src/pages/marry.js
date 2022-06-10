0661778aa3e198dc4e210bd18f434396093ac4ae
Felipe Schiavini <felipe@stabel.com.br>
2022-06-10 00:00:19 -0300

    mais perguntas

---
+++ src/pages/marry.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="Bora Neno !!!">
+        <iframe src="https://giphy.com/embed/gaJPlAdO21ns6Z2M3e" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/lifetimetv-mafs-married-at-first-sight-lifetime-gaJPlAdO21ns6Z2M3e"></a></p>
+        <iframe src="https://giphy.com/embed/ge2Ww4gBDZNhaUyJg5" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/shameless-episode-12-season-10-1012-ge2Ww4gBDZNhaUyJg5"></a></p>          <img width='240px' src="https://c.tenor.com/QhDzJP8-GIsAAAAC/mimi-neko.gif" alt="giff"></img>
+        <iframe src="https://giphy.com/embed/MXXnurORT4D6Hk0lu6" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/thelonelyisland-spring-break-lonely-island-lonelyisland-MXXnurORT4D6Hk0lu6"></a></p>          <img width='240px' src="https://c.tenor.com/QhDzJP8-GIsAAAAC/mimi-neko.gif" alt="giff"></img>
+        <iframe src="https://giphy.com/embed/lOsZiZtBywjwmqt4Qb" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/tlc-network-lOsZiZtBywjwmqt4Qb"></a></p>
+    </Layout>
+  )
+}
+export default AboutPage
